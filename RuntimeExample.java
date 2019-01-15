ee48583d974f32590942ffd222d1e4ec44ac4423
Eve Lin <evelin.dicheva@gmail.com>
2019-01-15 19:58:47 +0200

    Java Runtime Singleton Example

---
+++ RuntimeExample.java
@@ -0,0 +1,18 @@
+
+public class RuntimeExample {
+	
+	public static void main(String [] args) {
+		Runtime singletonRuntime = Runtime.getRuntime();
+		
+		singletonRuntime.gc();
+		System.out.println(singletonRuntime);
+		
+		Runtime anotherInRuntime = Runtime.getRuntime();
+		System.out.println(anotherInRuntime);
+		
+		if(singletonRuntime == anotherInRuntime) {
+			System.out.println("They are the same");
+		}
+	}
+
+}
